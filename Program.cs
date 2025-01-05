1c863b0517728cfd81ed1771f710cb27be9320f2
taksan <taksantong@gmail.com>
2025-01-05 17:49:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+
+namespace MyProject;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*Start");
+        GetAccountBalance().Wait();
+        Console.WriteLine("*End");
+    }
+    static async Task GetAccountBalance()
+    {
+        var web3 = new Web3("http://137.131.180.21:8545");
+        var balance = await web3.Eth.GetBalance.SendRequestAsync("0x8f401f28a4993894EFA3f39f962c34D33e16F9cB");
+        Console.WriteLine($"Balance in Wei: {balance.Value}");
+
+        var etherAmount = Web3.Convert.FromWei(balance.Value);
+        Console.WriteLine($"Balance in Ether: {etherAmount}");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# client_example_dotnet
-Example client connecting to a blockchain using dotnet
+# Example of C# connecting to a blockchain
+
+Using the [Nethereum](https://docs.nethereum.com/en/latest/nugets/) web3 package
+
+Add package with
+```
+dotnet add package Nethereum.Web3
+```
+
+The example code was taken from the Nethereum website. It connects to a sandbox blockchain (deployed at http://137.131.180.21:8545) and reads the balance of one of the test accounts
